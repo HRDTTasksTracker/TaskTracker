f51d02cd69e99238bb1d9c1600973995a1ebaaa7
mpmorgan94 <mpmorgan94@gmail.com>
2021-03-03 12:55:50 -0600

    user account requirments (password length >= 6, username length >= 6, email, and full name field.)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :username, presence: true
+    validates :username, length: { minimum: 6 }
+    validates :email, presence: true
+    validates :password, presence: true
+    validates :password, length: { minimum: 6 }
+    validates :full_name, presence: true
 end
