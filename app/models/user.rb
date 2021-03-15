b6cb537ba912a0b125c037a9e77a1e1f7614551f
mpmorgan94 <mpmorgan94@gmail.com>
2021-03-15 15:19:08 -0500

    Admins can edit all fields on all users, but cannot see other users passwords. Moderators can edit roles of all users, but not make anyone admins, they also can not see any info and can only edit role field. Moderators can not change admin accounts. When making a new account the role must be member. Members can only edit their own account and can not change their role to moderator or admin. Members are the only one that can see their password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true, length: { minimum: 6 }
     validates :email, presence: true
+    validates :role, presence: true
     validates :password, presence: true, length: { minimum: 6 }
     validates :full_name, presence: true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+    before_validation :strip_whitespace
+
+    private
+    def strip_whitespace
+        self.email = self.email.strip unless self.email.nil?
+        self.role = self.role.strip unless self.role.nil?
+    end
 end
