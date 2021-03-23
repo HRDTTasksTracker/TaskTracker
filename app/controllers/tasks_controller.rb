f70a603806c1e7105761ddf99d13449eb1e87b0a
mpmorgan94 <mpmorgan94@gmail.com>
2021-03-22 23:22:08 -0500

    reverted

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,7 @@
 class TasksController < ApplicationController
   def index
     # Returns all tasks in order by id
-    @tasks = Task.order(id: :asc)   #Returns all nodes stored in the nodes table
-    @nodes = Node.order(id: :asc)
-    @graph = Graph.new  #new graph tree created
-    @nodes.each do |row|
-      @graph.add_node(row)      #adding the node to graph
-
-
-    end
-
-    #now graph object contains all the nodes( task name , child task id) within the object.  
-
-      
-
+    @tasks = Task.order(id: :asc)
   end
 
   def show
@@ -27,11 +15,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      @node = Node.new(node_params)
-      @node.update(adjacent_nodes: [@task.child_task_id])
-      @node.save
       redirect_to @task
-      
     else
       render :new
     end
@@ -61,27 +45,4 @@ class TasksController < ApplicationController
     def task_params
       params.require(:task).permit(:task_name, :claimed_by, :child_task_id, :due_date, :task_description, :tag)
     end
-    def node_params
-      params.require(:task).permit(:task_name)
-    end
-    def node_params1
-      params.require(:node).permit(:task_name)
-    end
-  
-    class Graph
-      def initialize
-        @vertices = {}
-      end
-        
-      def add_node(vertex)
-        @vertices[vertex] = vertex
-      end
-        
-      def add_edge(node1, node2)
-        @vertices[node1].update(adjacent_nodes: node2.id)
-        @vertices[node2].add_edge(@nodes[node1])
-      end
-        
-    end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 2021_03_21_193013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "nodes", force: :cascade do |t|
-    t.string "task_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "adjacent_nodes", default: [], array: true
-  end
-
   create_table "roles", force: :cascade do |t|
     t.string "role_name"
     t.datetime "created_at", precision: 6, null: false
