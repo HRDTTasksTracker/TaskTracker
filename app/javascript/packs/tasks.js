23777706064b72ba6c734e9dacfcaaba9a9bb27c
timcampy118 <timothyn118@gmail.com>
2021-04-11 18:00:18 -0500

    minor bug fix

--- app/javascript/packs/tasks.js
+++ app/javascript/packs/tasks.js
@@ -2,183 +2,100 @@ window.addEventListener("load", function() {
   "use strict";
 
 
-var oldButton=document.getElementById('addButton');
-var oldLine;
-var elem = 1;
-
-document.getElementById('addButton').addEventListener('click', addButtonToContainer);
-
-
-function addButtonToContainer() {
-    const container = document.getElementById('container');
-    var button1 = document.createElement('button');
-    button1.innerText = 'Task '+ elem;
-    elem = elem+1;
-
-    container.appendChild(button1);
-    var lineTim;
-
- 
-
- lineTim = new LeaderLine(button1, oldButton, {
-    startPlug: "behind",
-    endPlug: "behind",
-    size: 4,
-    startPlugSize: 1,
-    endPlugSize: 1,
-    startSocket: "left",
-    endSocket: "right",    
-    color: "#30c117"
-    // path: 'grid',
-    // dropShadow: {color: '#111', dx: 0, dy: 2, blur: 0.2}
-  });
-
-
-    new PlainDraggable(button1, {
-    onMove: function() {
-      lineTim.position();
-      oldLine.position();
-    },
-    // onMoveStart: function() { line.dash = {animation: true}; },
-  
-  });
 
     
+    var nodesList = document.getElementsByClassName("node");
+    var connection = [];
+    var lines = [];
+    console.log(nodesList);
 
- oldButton=button1;
- oldLine=lineTim;
 
+
+      //creating the array of tasks
+       for (var i = 0; i < nodesList.length; i++) 
+      {
+        connection[i]=[];
+
+      }
+
+
+      //assigning each task their child and parent
+      for (var i = 0; i < nodesList.length; i++) 
+      {
+        var tmp = nodesList[i].getElementsByClassName('node-name')[0].id
+        if(tmp)
+        {
+            connection[tmp-1].push(i);
+            connection[i].push(tmp-1);
+          }
+      }
+
+
+      //creating the lines
+      for (var x = 0; x < connection.length; x++) 
+      {
+        console.log()
+        for (var y = 0; y < connection[x].length; y++) 
+        {
+            console.log(document.getElementsByClassName("name active")[x*2])
+            console.log(document.getElementsByClassName("name active")[x*connection[x][y]+1])
+
+
+
+          var tmpLine = new LeaderLine(document.getElementsByClassName("name active")[x*2], document.getElementsByClassName("name active")[x*connection[x][y]+1], {
+            startPlug: "behind",
+            endPlug: "behind",
+            size: 4,
+            startPlugSize: 1,
+            endPlugSize: 1,
+            startSocket: "left",
+            endSocket: "right",    
+            color: "#30c117"
+
+          });
+          console.log(1);
+          lines.push(tmpLine);
+        }
+
+      }
+
+
+      for (var x = 0; x < nodesList.length; x++) 
+      {
+         new PlainDraggable(nodesList[x], {
+          onMove: function() {
+            for (var y = 0; y < lines.length; y++) 
+              {
+              lines[y].position();
+              }
+          },
+
+        });
+
+
+      }
+
+
+
+
+
+
+
+
+
+      
+
+     
+      console.log(lines);
+      
+    
+  
+function myFunction(elem){
+  document.location.href = "https://powerful-waters-96115.herokuapp.com/tasks/"+elem;
 }
 
-  var slot1_out = document.getElementById("slot-anchor-4"),
-    slot2_in = document.getElementById("slot-anchor-2-tim"),
-    slot3_in = document.getElementById("slot-anchor-3-tim"),
-    slot4_out = document.getElementById("slot-anchor-4"),
-    slot5_in = document.getElementById("slot-anchor-5"),
 
 
-    node3_out = document.getElementById("slot-anchor-3-out-tim"),
-    node4_out = document.getElementById("slot-anchor-5-out"),
-    node5_in = document.getElementById("node_5_tim_in"),
-    
-    slot2_out = document.getElementById("slot-anchor-2-out-tim"),
 
-    node_4_slot1_in = document.getElementById("node_4_slot1_in"),
-    lt_slot1_out = document.getElementById("lt_slot1_out"),
-    line1,
-    line2,
-    line3,
-    line4,
-    line5;
-
-
-      line1 = new LeaderLine(slot2_in, slot1_out, {
-    startPlug: "behind",
-    endPlug: "behind",
-    size: 4,
-    startPlugSize: 1,
-    endPlugSize: 1,
-    startSocket: "left",
-    endSocket: "right",
-    color: "#30c117"
-   
-  });
-  line2 = new LeaderLine(slot3_in, slot2_out, {
-    startPlug: "behind",
-    endPlug: "behind",
-    size: 4,
-    startPlugSize: 1,
-    endPlugSize: 1,
-    startSocket: "left",
-    endSocket: "right",
-    color: "#30c117"
- 
-  });
-
-  
-  line3 = new LeaderLine(slot5_in, slot4_out, {
-    startPlug: "behind",
-    endPlug: "behind",
-    size: 4,
-    startPlugSize: 1,
-    endPlugSize: 1,
-    startSocket: "left",
-    endSocket: "right",
-    color: "#30c117",
-   
-  });
-
-   line4 = new LeaderLine(node3_out, node_5_tim_in, {
-    startPlug: "behind",
-    endPlug: "behind",
-    size: 4,
-    startPlugSize: 1,
-    endPlugSize: 1,
-    startSocket: "left",
-    endSocket: "left",
-    color: "#30c117",
-    
-  });
-
-  line5 = new LeaderLine(node4_out, node_5_tim_in, {
-    startPlug: "behind",
-    endPlug: "behind",
-    size: 4,
-    startPlugSize: 1,
-    endPlugSize: 1,
-    startSocket: "left",
-    endSocket: "right",
-    color: "#30c117",
-    
-  });
-
-  // Drag Nodes
-
-  // Drag Nodes and redraw lines
-  new PlainDraggable(node_1, {
-    onMove: function() {
-      line1.position();
-      line2.position();
-      line3.position();
-    },
-
-  });
-
-  new PlainDraggable(node_2, {
-    onMove: function() {
-      line1.position();
-      line2.position();
-    },
-    // onMoveStart: function() { line.dash = {animation: true}; },
-    onDragEnd: function() {
-      line.dash = false;
-    }
-  });
-
-  new PlainDraggable(node_3, {
-    onMove: function() {
-      line1.position();
-      line2.position();
-      line3.position();
-      line4.position();
-    }
-  });
-  new PlainDraggable(node_4, {
-    onMove: function() {
-      line3.position();
-      line5.position();
-    },
-
-  });
-
-
-  new PlainDraggable(node_5_tim, {
-    onMove: function() {
-      line4.position();
-      line5.position();
-    },
-
-  });
-  
   
 });
