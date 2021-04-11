b690b1024c6ed0d80f7d3a5e43614450f0add191
samakshsingh7 <53239467+samakshsingh7@users.noreply.github.com>
2021-04-11 18:26:49 -0500

    Add files via upload

---
+++ db/migrate/20210321190430_create_nodes.rb
@@ -0,0 +1,13 @@
+class CreateNodes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :nodes do |t|
+      t.string "task_name"
+      add_column :nodes, :adjacent_nodes, :integer, array: true, default: []
+
+      t.timestamps
+    end
+  end
+end
+
+
+
