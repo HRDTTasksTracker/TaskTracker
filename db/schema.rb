23912f43a50f02b338676f5ca6afc6a11ac0a325
mpmorgan94 <mpmorgan94@gmail.com>
2021-04-12 02:48:53 -0500

    single qutoes on db schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,42 +13,42 @@
 ActiveRecord::Schema.define(version: 2021_03_21_193013) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "roles", force: :cascade do |t|
-    t.string "role_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'roles', force: :cascade do |t|
+    t.string 'role_name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "tasks", force: :cascade do |t|
-    t.integer "claimed_by"
-    t.string "task_name"
-    t.text "task_description"
-    t.datetime "due_date"
-    t.integer "child_task_id"
-    t.string "tag"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'tasks', force: :cascade do |t|
+    t.integer 'claimed_by'
+    t.string 'task_name'
+    t.text 'task_description'
+    t.datetime 'due_date'
+    t.integer 'child_task_id'
+    t.string 'tag'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "full_name"
-    t.string "role"
-    t.string "email"
-    t.string "discord_username"
-    t.string "phone"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "username"
-    t.string "password"
+  create_table 'users', force: :cascade do |t|
+    t.string 'full_name'
+    t.string 'role'
+    t.string 'email'
+    t.string 'discord_username'
+    t.string 'phone'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'username'
+    t.string 'password'
   end
 
-  create_table "valid_emails", force: :cascade do |t|
-    t.string "email"
-    t.boolean "in_use"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'valid_emails', force: :cascade do |t|
+    t.string 'email'
+    t.boolean 'in_use'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
 end
