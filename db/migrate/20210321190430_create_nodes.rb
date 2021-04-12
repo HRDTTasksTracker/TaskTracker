6ecb57a840f364ff608fef9e025e06c418c04ae3
samakshsingh7 <53239467+samakshsingh7@users.noreply.github.com>
2021-04-11 20:04:38 -0500

    Update 20210321190430_create_nodes.rb

--- db/migrate/20210321190430_create_nodes.rb
+++ db/migrate/20210321190430_create_nodes.rb
@@ -2,7 +2,7 @@ class CreateNodes < ActiveRecord::Migration[6.1]
   def change
     create_table :nodes do |t|
       t.string "task_name"
-      add_column :nodes, :adjacent_nodes, :integer, array: true, default: []
+      #add_column :nodes, :adjacent_nodes, :integer, array: true, default: []
 
       t.timestamps
     end
